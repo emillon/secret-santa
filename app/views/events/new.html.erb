f666d3f2e37cd33df5ff2aa294731715169a2dc1
Etienne Millon <me@emillon.org>
2014-12-03 14:44:24 +0100

    New event page

---
+++ app/views/events/new.html.erb
@@ -0,0 +1,1 @@
+<h1>Create an event</h1>
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,5 +5,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
+    assert_select 'a[href=?]', new_event_path
+
+    get new_event_path
+    assert_template 'events/new'
   end
 end
