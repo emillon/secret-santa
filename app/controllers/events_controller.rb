5950a1a56be49391531b9a3d11dbffe5ee3e17b8
Etienne Millon <me@emillon.org>
2014-12-03 16:00:15 +0100

    valid event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,11 +4,21 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new
+    @event = Event.new event_params
     if @event.save
-      pass
+      redirect_to @event
     else
       render 'new'
     end
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:title)
+    end
 end
--- test/integration/events_creation_test.rb
+++ test/integration/events_creation_test.rb
@@ -9,4 +9,13 @@ class EventsCreationTest < ActionDispatch::IntegrationTest
     assert_template 'events/new'
     assert_select 'div#error_explanation'
   end
+
+  test "valid event creation" do
+    get new_event_path
+    assert_difference 'Event.count', 1 do
+      post events_path, event: { title:  "Secret Santa at my place" }
+    end
+    follow_redirect!
+    assert_template 'events/show'
+  end
 end
