147e1d91887fdfbd036b54ea5386f1c02a9f3d0b
Etienne Millon <me@emillon.org>
2014-12-03 18:41:20 +0100

    test valid edit

--- test/integration/events_edit_test.rb
+++ test/integration/events_edit_test.rb
@@ -6,13 +6,31 @@ class EventsEditTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid edit with bad title" do
-    get edit_event_path(@event)
-    assert_template 'events/edit'
     patch event_path(@event), event: {
       title: ''
     }
     assert flash.empty?
     assert_template 'events/edit'
     assert_select 'div#error_explanation'
+  end
+
+  test "valid edit" do
+    get edit_event_path(@event)
+    assert_template 'events/edit'
+    patch event_path(@event), event: {
+      participants_attributes: {
+        '1' => { name: 'a',
+                 email: 'a@example.com',
+                 _destroy: 'false'
+               },
+        '2' => { name: 'b',
+                 email: 'b@example.com',
+                 _destroy: 'false'
+               }
+      }
+    }
+    follow_redirect!
+    assert_not flash.empty?
+    assert_template 'events/show'
   end
 end
