08282b1fe95f1002c744b736342334609b9bb022
Etienne Millon <me@emillon.org>
2014-12-04 18:17:35 +0100

    better email templates

--- test/mailers/draw_mailer_test.rb
+++ test/mailers/draw_mailer_test.rb
@@ -10,7 +10,10 @@ class DrawMailerTest < ActionMailer::TestCase
     assert_equal "Secret Santa - gift assignment for Michel", mail.subject
     assert_equal [michel.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match "to Bob", mail.body.encoded
+    assert_match "Hello Michel", mail.html_part.decoded
+    assert_match "Hello Michel", mail.text_part.decoded
+    assert_match /to.*Bob/m, mail.html_part.decoded
+    assert_match /to.*Bob/m, mail.text_part.decoded
   end
 
 end
