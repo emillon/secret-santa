ab9079748d86e99d1c35890ef365c273d21d5c76
Etienne Millon <me@emillon.org>
2014-12-04 17:00:24 +0100

    fix title

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,11 +8,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', root_path
     assert_select 'a[href=?]', new_event_path, count: 2
     assert_select 'a[href=?]', help_path
+    assert_select 'title', 'Secret Santa creator'
 
     get new_event_path
     assert_template 'events/new'
     assert_select 'a[href=?]', root_path
     assert_select 'a[href=?]', new_event_path
     assert_select 'a[href=?]', help_path
+    assert_select 'title', 'Secret Santa creator'
   end
 end
