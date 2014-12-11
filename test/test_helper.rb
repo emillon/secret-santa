1f10c65965e6b7072c76e480748a4bd71ee0ea42
Etienne Millon <me@emillon.org>
2014-12-11 23:10:47 +0100

    require coveralls before anything else

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
+require 'coveralls'
+Coveralls.wear!('rails')
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use!
-require 'coveralls'
-Coveralls.wear!('rails')
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
