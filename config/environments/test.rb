82ab385d776900a5519a96195cc4b7da0cdeff3b
Etienne Millon <me@emillon.org>
2016-05-29 22:13:30 +0200

    Use config.public_file_server.headers

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,11 +15,11 @@ Rails.application.configure do
   # Configure static asset server for tests with Cache-Control for performance.
   if Rails::VERSION::MAJOR >= 5
     config.public_file_server.enabled = true
+    config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
   else
     config.serve_static_files = true
+    config.static_cache_control = 'public, max-age=3600'
   end
-
-  config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
